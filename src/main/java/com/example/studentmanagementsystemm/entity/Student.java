70e2abf05a54cff339f2cef868fe5bda6db68382
pelinhangisi <pelinhangisi@gmail.com>
2022-06-22 13:58:12 +0300

    Anatasyonlar ile ilgili düzenlemeler yapıldı

--- src/main/java/com/example/studentmanagementsystemm/entity/Student.java
+++ src/main/java/com/example/studentmanagementsystemm/entity/Student.java
@@ -1,10 +1,17 @@
 package com.example.studentmanagementsystemm.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
 @Table(name = "students") //The part that provides the creation of the table name in the database connection
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     @Id
@@ -22,46 +29,5 @@ public class Student {
     private String email;
 
 
-    //Constuctor Methods
-    public Student(){}
 
-    public Student(String firstName, String lastName, String email) {
-        super();
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    // Fields Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
